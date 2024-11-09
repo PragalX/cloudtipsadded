e9f8127bff32ff49c61344aac01e5c8ed2b46332
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-11-09 12:47:01 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -93,21 +93,4 @@ export default function Home() {
     </motion.div>
   );
 }
-// Previous imports remain the same
-import CloudSection from '../components/CloudSection';
 
-const Home = () => {
-  return (
-    <>
-      <CloudSection />
-
-      <motion.footer
-      // ... rest of the code remains the same
-      >
-        {/* Footer content goes here */}
-      </motion.footer>
-    </>
-  );
-};
-
-export default Home;
